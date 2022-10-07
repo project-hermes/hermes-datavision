3bcc014d911e2554d03af4221cd9063dac478bee
Michael Marzouk <michael@wirk.io>
2022-10-07 09:27:04 +0200

    1st commit

---
+++ hermes-datavision/hermes-datavision/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hermes_datavision.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
